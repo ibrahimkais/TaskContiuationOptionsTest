1ad9a2d826145682203a3af050b8f0d3e6a966a3
Ibrahim Kais <ibrahimkais@hotmail.com>
2021-10-23 15:51:51 +0200

    I added the WhenAll function to guarantee the press any key message will be written after all the continuation tasks are finished

--- TaskContiuationOptionsTest/Program.cs
+++ TaskContiuationOptionsTest/Program.cs
@@ -19,18 +19,24 @@ namespace TaskContiuationOptionsTest
                     Console.WriteLine("Parent task");
                     await Task.Delay(random.Next(1, 10));
                     cancellationToken.ThrowIfCancellationRequested();
-                    _ = 5 / random.Next(0, 4);
+                    _ = 5 / random.Next(0, 3);
                 }, cancellationToken);
 
-                _ = task.ContinueWith(_ => Console.WriteLine("Ran To Completion 1"), TaskContinuationOptions.OnlyOnRanToCompletion);
-                _ = task.ContinueWith(_ => Console.WriteLine("Ran To Completion 2"), TaskContinuationOptions.OnlyOnRanToCompletion);
-                _ = task.ContinueWith(_ => Console.WriteLine("Ran To Completion 3"), TaskContinuationOptions.OnlyOnRanToCompletion);
-                _ = task.ContinueWith(_ => Console.WriteLine("Faulted"), TaskContinuationOptions.OnlyOnFaulted);
-                _ = task.ContinueWith(_ => Console.WriteLine("Canceled"), TaskContinuationOptions.OnlyOnCanceled);
+                Task task1 = task.ContinueWith(_ => Console.WriteLine("Ran To Completion 1"), TaskContinuationOptions.OnlyOnRanToCompletion);
+                Task task2 = task.ContinueWith(_ => Console.WriteLine("Ran To Completion 2"), TaskContinuationOptions.OnlyOnRanToCompletion);
+                Task task3 = task.ContinueWith(_ => Console.WriteLine("Ran To Completion 3"), TaskContinuationOptions.OnlyOnRanToCompletion);
+                Task task4 = task.ContinueWith(_ => Console.WriteLine("Faulted"), TaskContinuationOptions.OnlyOnFaulted);
+                Task task5 = task.ContinueWith(_ => Console.WriteLine("Canceled"), TaskContinuationOptions.OnlyOnCanceled);
 
                 try { await task; } catch { }
-                Console.WriteLine("{0}Press any Key to try again{0}", Environment.NewLine);
-                Console.ReadKey();
+
+                await Task.WhenAll(new[] { task1, task2, task3, task4, task5 }).ContinueWith(_ =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("{0}Press any Key to try again{0}", Environment.NewLine);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                });
             }
         }
     }
